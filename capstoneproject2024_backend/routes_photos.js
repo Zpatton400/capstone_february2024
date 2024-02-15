03e727f9ac6d4a8f3b846416b4277c21b668249f
Zena Patton <147436544+Zpatton400@users.noreply.github.com>
2024-02-15 09:16:41 -0500

    Delete capstoneproject2024_backend directory
    I will update with a new directory.

--- capstoneproject2024_backend/routes_photos.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Photo = require('../models/Photo');
-
-// Define routes for CRUD operations here
-
-module.exports = router;
